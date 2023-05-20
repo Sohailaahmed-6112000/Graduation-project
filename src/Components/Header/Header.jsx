935632743d611b27998c480a6b1f3ae8a4a38de0
Mano276 <aymanmanar385@gmail.com>
2023-05-20 20:05:55 +0300

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,4 @@
 
-import React from 'react';
-// import {AiFillYoutube} from 'react-icons/ai'; // import your logo image
-// import { CgProfile} from 'react-icons/cg'; // import your profile picture image
-// import {IoMdNotifications} from 'react-icons/io'
-
-
 import React, { useState } from "react";
 import { AiFillYoutube, AiOutlineBars } from "react-icons/ai"; // import your logo image
 import { CgProfile } from "react-icons/cg"; // import your profile picture image
